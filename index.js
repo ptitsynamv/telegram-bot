6e7632b24351e02494a2df4e68fa6469d5c6a6eb
ptitsynamasha <ptitsynamv@gmail.com>
2018-12-19 13:45:32 +0200

    Add Meters on hts.kharkov site

--- index.js
+++ index.js
@@ -70,3 +70,45 @@ cron.schedule("1 1 20 20,21,22,23,24,25 * *", function () {
             }
         );
 });
+
+
+const url = 'https://www.hts.kharkov.ua/KPHTS_v2_bill1PU.php?pg=1';
+const Horseman = require('node-horseman');
+const horseman = new Horseman();
+const email = 'ptitsynamv@gmail.com';
+const password = 'akindofmagic12';
+
+
+function horsemanStart(email, password) {
+    horseman
+        .userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36')
+        .open(url)
+        .type('input[type=email]', email)
+        .type('input[type=password]', password)
+        .click('.message-block input[type="submit"]')
+        .waitForNextPage()
+
+        .text('.Header1')
+        .then((text) => {
+            console.log('text', text);
+        })
+        .click("a[href='KPHTS_v2_bill01PUNrazvilka.php']")
+        .waitForNextPage()
+
+        .text('div .ui-state-default')
+        .then((text) => {
+            console.log('text', text);
+        })
+        .type('input[name="0[ValueN]"]', 'test')
+        .type('input[name="1[ValueN]"]', 'test')
+        .click('input[type="submit"]')
+
+        .waitForSelector('.info_errors')
+        .text('.info_errors')
+        .then((text) => {
+            console.log('text', text);
+        })
+        .close();
+}
+
+// horsemanStart(email, password);
