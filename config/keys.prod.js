b0e0cb49f9918dd264f2f3fe2b386eb1c0abac23
ptitsynamasha <ptitsynamv@gmail.com>
2018-12-20 13:32:49 +0200

    keys prod and dev

---
+++ config/keys.prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    telegramToken: process.env.TELEGRAM_TOKEN,
+    idMasha: process.env.ID_MASHA,
+    mongoUrl: process.env.MONDO_URI,
+    emailVodocanal: process.env.EMAIL_VODOCANAL,
+    passwordVodocanal: process.env.PASSWORD_VODOCANAL,
+};
