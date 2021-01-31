eac2762fc5ad67ca9f02221c44ca249d7bb98b2a
maria <ptitsynamv@gmail.com>
2021-01-31 22:02:14 +0200

    remove DB

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const TelegramBot = require('node-telegram-bot-api');
 const scraping = require('./utils/scraping');
@@ -6,15 +6,15 @@ const {from} = require('rxjs');
 const {concatMap, map} = require('rxjs/operators');
 const Horseman = require('node-horseman');
 const horseman = new Horseman();
-const Article = require('./models/Article');
+// const Article = require('./models/Article');
 
 
-try {
-    mongoose.connect(keys.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-        console.log("mongoose connected"));
-} catch (error) {
-    console.log("could not connect");
-}
+// try {
+//     mongoose.connect(keys.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+//         console.log("mongoose connected"));
+// } catch (error) {
+//     console.log("could not connect");
+// }
 
 const bot = new TelegramBot(keys.TELEGRAM_BOT_TOKEN, {polling: true});
 
@@ -26,34 +26,44 @@ bot.onText(/\/help/, (msg, match) => {
 
 bot.onText(/\/hi/, (msg, match) => {
     const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `please, wait...`);
+    const data = [
+        {
+            title: 'Королевская канарейка',
+            url: 'https://ficbook.net/readfic/9500650',
+        },
+        {
+            title: 'Ава',
+            url: 'https://ficbook.net/readfic/4582645',
+        },
+    ]
 
-    bot.sendMessage(chatId,`please, wait...`);
-
-    Article.find({}, 'title url')
-        .then((data) => {
-            from(data)
-                .pipe(
-                    map(({title, url}) => ({title, url})),
-                    concatMap((d) => scraping.scrapingArticles(d, horseman))
-                )
-                .subscribe(
-                    (result) => {
-                        bot.sendMessage(chatId,`article: ${result.title},
+    from(data)
+        .pipe(
+            // map(({title, url}) => ({title, url})),
+            concatMap((d) => scraping.scrapingArticles(d, horseman))
+        )
+        .subscribe(
+            (result) => {
+                bot.sendMessage(chatId, `article: ${result.title},
 url: ${result.url},
 last updated: ${result.diff} days ago`)
-                    },
-                    (error) => {
-                    },
-                    () => {
-                        return horseman.close();
-                    },
-                );
+            },
+            (error) => {
+            },
+            () => {
+                return horseman.close();
+            },
+        );
 
-        });
+    // todo from DB
+    // Article.find({}, 'title url')
+    //     .then((data) => {
+    //     });
 });
 bot.onText(/\/add/, (msg, match) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId,`todo`)
+    bot.sendMessage(chatId, `todo`)
 
 
     // const article = new Article({
